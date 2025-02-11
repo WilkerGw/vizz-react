346d4108d87a4277e12cddcd3e952e696f6e70a2
Wilker Martins <dev.wilker@gmail.com>
2025-02-11 18:08:35 -0300

    ajuste

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'; // Importe o React explicitamente
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
